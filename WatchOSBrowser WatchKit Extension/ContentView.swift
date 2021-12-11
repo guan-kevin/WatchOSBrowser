1c8ce91351e76a000736af13cad29c94e60b28c9
guan-kevin <github@pigzy.net>
2021-12-10 22:59:53 -0800

    Initial Commit

---
+++ WatchOSBrowser WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WatchOSBrowser WatchKit Extension
+//
+//  Created by Kevin on 12/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WatchOSBrowser WatchKit Extension/WatchOSBrowserApp.swift
@@ -0,0 +1,19 @@
+//
+//  WatchOSBrowserApp.swift
+//  WatchOSBrowser WatchKit Extension
+//
+//  Created by Kevin on 12/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WatchOSBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
